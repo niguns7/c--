5fce58418095d2e7301945c8a9c7888b8e377ff4
niguns7 <nirja767@gmail.com>
2022-12-05 17:15:17 +0545

    more programms

---
+++ 2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout<<"enter your name:\n";
+    cin>>name;
+    cout<<"my name is "<<name<<"\n";
+    return 0;
+}
---
+++ 3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int x= 10;
+    char y= 'a';
+    int a=int(y);
+
+    cout<<a;
+    return 0;
+}
