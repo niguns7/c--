497bbd5b588f38236b22def6934b8c81c4daf1fb
niguns7 <nirja767@gmail.com>
2022-12-05 17:15:07 +0545

    first c++ code

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout<<setw(20)<<"Banepaaa \n"<<endl;
+    cout<<setw(20)<<"Bhaktapur \n"<<endl;
+    return 0;
+    
+}
